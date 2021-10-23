4a0aa40b7fc20ffd5371c7807e2fae5acffc86ca
Daniella Francine Velasquez <daniellafrancine.velasquez@gmail.com>
2021-10-23 23:21:27 +0800

    changes

--- program2.py
+++ program2.py
@@ -3,3 +3,5 @@ apple_price = 20
 orange_price = 25
 apple = input("Please indicate how many apples you want to purchase: ")
 orange = input("Please indicate how many oranges you want to purchase: ")
+apple_str= int(apple)
+orange_str= int(orange)
