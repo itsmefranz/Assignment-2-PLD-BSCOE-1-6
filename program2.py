3646088d8e4dfd9bd85bf479d417f61cae4ee81f
Daniella Francine Velasquez <daniellafrancine.velasquez@gmail.com>
2021-10-23 23:10:56 +0800

    second upload

---
+++ program2.py
@@ -0,0 +1,5 @@
+print("The price of the apples is ₱20 and the price of the oranges is ₱25.")
+apple_price = 20
+orange_price = 25
+apple = input("Please indicate how many apples you want to purchase: ")
+orange = input("Please indicate how many oranges you want to purchase: ")
