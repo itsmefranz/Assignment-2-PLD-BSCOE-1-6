145fccbf29e75dc336d948d30205c87782a51de4
Daniella Francine Velasquez <daniellafrancine.velasquez@gmail.com>
2021-10-23 23:41:32 +0800

    third upload

---
+++ program3.py
@@ -0,0 +1,6 @@
+money = float(input("How much money do you have? "))
+apple = int(input("What is the price of an apple per piece? "))
+
+maximumNumberApples= money // apple
+
+change = money % apple
