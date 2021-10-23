c77eae408e98a882ab812323cc5ef28be53ee1e2
Daniella Francine Velasquez <daniellafrancine.velasquez@gmail.com>
2021-10-23 23:18:06 +0800

    code complete

--- program1.py
+++ program1.py
@@ -1,3 +1,5 @@
-name=input("Enter Name")
-age=int(input("Enter Age"))
-address=input("Enter Address")
+name =input("Enter Name ")
+age =int(input("Enter Age "))
+address =input("Enter Address ")
+
+print=(f'Hi, my name is {name}. I am {age} years old and I live in {address}')
