67e203f90c0c3d34606740f9bef7320f6bd5bb2a
Daniella Francine Velasquez <daniellafrancine.velasquez@gmail.com>
2021-10-23 23:44:39 +0800

    code complete

--- program3.py
+++ program3.py
@@ -4,3 +4,4 @@ apple = int(input("What is the price of an apple per piece? "))
 maximumNumberApples= money // apple
 
 change = money % apple
+print(f"You can buy {maximumNumberApples} apples and your change is ₱{change: .2f}.")
