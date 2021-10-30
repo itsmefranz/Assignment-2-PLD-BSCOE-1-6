29769a9a7c78e721a3e470d5344830e8605d8b8f
Daniella Francine Velasquez <daniellafrancine.velasquez@gmail.com>
2021-10-30 22:25:58 +0800

    first upload

---
+++ program1_v2.py
@@ -0,0 +1,17 @@
+
+def getName():
+    _name= input("Name: ")
+    return _name 
+
+
+
+#steps
+#1. ask for name then save as variable
+name= getName()
+#2. ask for age then save as variable
+age= getAge()
+#3. ask for address then save as variable
+
+#4. display the details
+
+display(name, age, address)
